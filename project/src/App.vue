a3cba49a9660216e5291c06a352e3578798e158f
Ester79 <esterramoscuervo@hotmail.com>
2022-09-04 19:02:14 +0200

    Iteration 1: Navbar / footer components

---
+++ project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div>
+    <h1>This is my Form</h1>
+</div>
+
+<navbar-vue></navbar-vue>
+<footer-vue></footer-vue>
+
+</template>
+
+<script>
+
+import navbarVue from './components/navbar.vue';
+import footerVue from './components/footer.vue';
+
+
+
+
+export default {
+    name: 'App',
+    components: {
+    navbarVue,
+    footerVue
+}
+}
+</script>
+
+
+
+<style>
+</style>
---
+++ project/src/components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Footer component</h2>
+        <p>{{footerItem}}</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                footerItem: "Item footer"
+            }
+        }
+        
+    }
+
+
+</script>
+
+<style></style>
